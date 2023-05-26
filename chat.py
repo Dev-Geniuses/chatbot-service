95ed58cde4901c3caa037cd3ded023f1759d6108
letmetry1 <rafaelaguilarza146@gmail.com>
2023-05-25 23:34:00 -0500

    Salio mrd

--- chat.py
+++ chat.py
@@ -6,10 +6,10 @@ from haystack import Pipeline
 
 
 async def  init_chat():
+    
+    document_store = InMemoryDocumentStore(use_bm25=True)
 
-    document_store = await InMemoryDocumentStore(use_bm25=True)
-
-    preprocessor = await PreProcessor(
+    preprocessor =  PreProcessor(
         clean_whitespace=True,
         clean_header_footer=True,
         clean_empty_lines=True,
@@ -19,24 +19,16 @@ async def  init_chat():
         split_respect_sentence_boundary=True,
     )
 
-    reader = await FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
-    retriever_es = await BM25Retriever(document_store=document_store)
-    pipe = await ExtractiveQAPipeline(reader, retriever_es)
+    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
+    retriever_es = BM25Retriever(document_store=document_store)
+    pipe = ExtractiveQAPipeline(reader, retriever_es)
 
     pdf_file="./pdf2.pdf"
-    converter = await PDFToTextConverter(remove_numeric_tables=True, valid_languages=["es"])
-    document = await [converter.convert(file_path=pdf_file, meta=None)[0]]
-    preprocessed_docs = await preprocessor.process(document)
-    await document_store.write_documents(preprocessed_docs)
+    converter = PDFToTextConverter(remove_numeric_tables=True, valid_languages=["es"])
+    document = [converter.convert(file_path=pdf_file, meta=None)[0]]
+    preprocessed_docs = preprocessor.process(document)
+    document_store.write_documents(preprocessed_docs)
 
-    question= "Qué es un informe académico?"
-
-    result = await pipe.run(
-            query=question, 
-            params={
-                "Retriever": {"top_k": 10},
-                "Reader": {"top_k": 5}
-            })
-
-    print(result.get('answers')[0].answer) 
+    
+    return pipe
 
--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 from flask import Flask, request, jsonify;
 from flask_cors import cross_origin
-
+import asyncio
+import tracemalloc
 
 from haystack.nodes import FARMReader, PreProcessor, PDFToTextConverter, DensePassageRetriever
 from haystack.nodes import BM25Retriever
 from haystack.document_stores import InMemoryDocumentStore
 from haystack.pipelines import ExtractiveQAPipeline
 from haystack import Pipeline
-from chat import init_chat 
+from chat import init_chat
 
 
 app = Flask(__name__)
 
 
 
+global pipe
+
 @app.route('/chat_bot', methods = ['POST'])
 @cross_origin()
-def chat(request):
+def chat():
     if request.method == 'POST':
-        message_send = request.get_json()
-        
-        return jsonify({'message': result.get('answers')[0].answer})
+        message_send = request.json.get('message')
+        result = pipe.run(
+            query=message_send, 
+            params={
+                "Retriever": {"top_k": 10},
+                "Reader": {"top_k": 5}
+            })
+
+    return jsonify({'Answer': result.get('answers')[0].answer})
     
 
 async def main():
-    await init_chat()
-
+   return await init_chat()
+   
 
 if __name__ == "__main__":
-    main()
+
+    tracemalloc.start()
+    pipe = asyncio.run(main())
     app.run(debug=True)
 
